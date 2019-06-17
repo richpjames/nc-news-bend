d3ac1df334c09fd05513e9aaee72fc1dc1df82eb
ricojamjar <rich@genericgreeting.co.uk>
2019-06-17 16:58:00 +0100

    destructuring user properly

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,18 +1,19 @@
 const { getUsers } = require("../models/users-model");
 
 exports.fetchUsers = (req, res, next) => {
-  const { user_id } = req.params;
-  getUsers(user_id)
-    .then(user => {
-      if (user.length === 0) {
-        return Promise.reject({
-          status: 404,
-          msg: `No user found for user_id: ${user_id}`
+    const { user_id } = req.params;
+    getUsers(user_id)
+        .then(userArr => {
+            if (userArr.length === 0) {
+                return Promise.reject({
+                    status: 404,
+                    msg: `No user found for user_id: ${user_id}`
+                });
+            }
+            const user = {...userArr[0] };
+            res.status(200).send({ user });
+        })
+        .catch(err => {
+            next(err);
         });
-      }
-      res.status(200).send(user);
-    })
-    .catch(err => {
-      next(err);
-    });
 };
