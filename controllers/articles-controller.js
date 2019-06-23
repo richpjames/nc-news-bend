0bb5efe6f0e3069d13a78f7900ba0056b23c31da
ricojamjar <rich@genericgreeting.co.uk>
2019-06-23 21:25:37 +0100

    ensuring controller checks article exists before allowing comments to be posted

--- controllers/articles-controller.js
+++ controllers/articles-controller.js
@@ -12,7 +12,7 @@ exports.fetchArticlesById = (req, res, next) => {
   getArticlesById(articles_id)
     .then(articleArr => {
       const article = articleArr[0];
-      if (article.length === 0) {
+      if (articleArr.length === 0) {
         return Promise.reject({
           status: 404,
           msg: `No article found for article_id: ${articles_id}`
@@ -24,37 +24,20 @@ exports.fetchArticlesById = (req, res, next) => {
 };
 
 exports.postVotesForArticles = (req, res, next) => {
-  const increment = req.body.inc_votes;
+  let increment = Number(req.body.inc_votes);
   const { articles_id } = req.params;
-  if (increment > 0) {
-    updateVotes(articles_id, increment)
-      .then(articleArr => {
-        const article = articleArr[0];
-        if (article.length === 0) {
-          return Promise.reject({
-            status: 404,
-            msg: `No article found for article_id: ${articles_id}`
-          });
-        }
-        res.status(200).send({ article });
-      })
-      .catch(next);
+  if (increment !== -1 && increment !== 1) {
+    increment = 0;
   }
-};
-
-exports.postComment = (req, res, next) => {
-  const comment = req.body;
-  const { articles_id } = req.params;
-  insertComment(articles_id, comment)
-    .then(commentInArray => {
-      const [comment] = commentInArray;
-      if (comment.length === 0) {
+  updateVotes(articles_id, increment)
+    .then(articleArr => {
+      const article = articleArr[0];
+      if (articleArr.length === 0) {
         return Promise.reject({
           status: 404,
           msg: `No article found for article_id: ${articles_id}`
         });
-      }
-      res.status(201).send({ comment });
+      } else res.status(200).send({ article });
     })
     .catch(next);
 };
@@ -64,6 +47,26 @@ exports.fetchCommentsByArticleId = (req, res, next) => {
   getCommentsByArticleId(articles_id, req.query)
     .then(comments => {
       res.status(200).send({ comments });
+    })
+    .catch(next);
+};
+
+exports.postComment = (req, res, next) => {
+  const comment = req.body;
+  const { articles_id } = req.params;
+  getArticlesById(articles_id)
+    .then(response => {
+      if (response.length < 1) {
+        return Promise.reject({
+          status: 404,
+          msg: `No article found for article_id: ${articles_id}`
+        });
+      } else {
+        insertComment(articles_id, comment).then(commentInArray => {
+          const [comment] = commentInArray;
+          res.status(201).send({ comment });
+        });
+      }
     })
     .catch(next);
 };
